a8917d63a0e64de65a791a1ea384fdb080c044f3
Ann Markham <ann.markham@outlook.com>
2020-12-06 16:26:17 -0700

    finally fixed?

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -28,23 +28,23 @@ const handleAnimalFormSubmit = event => {
   const animalObject = { name, species, diet, personalityTraits };
 
 fetch('/api/animals', {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(animalObject)
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(animalObject)
+})
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    alert('Error: ' + response.statusText);
   })
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      alert(`Error: ${response.statusText}`);
-    })
-    .then(postResponse => {
-      console.log(postResponse);
-      alert('Thank you for adding an animal!');
-    });
+  .then(postResponse => {
+    console.log(postResponse);
+    alert('Thank you for adding an animal!');
+  });
 };
 
 $animalForm.addEventListener('submit', handleAnimalFormSubmit);
